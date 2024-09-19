b9f388b560e4d7db14742886d5f015bcc901cb87
Ayushighiaa <ghia.a@northeastern.edu>
2024-09-19 18:35:42 -0400

    assignment 1
    completed

---
+++ Assignment_1/src/assignment_1/Person.java
@@ -0,0 +1,198 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package assignment_1;
+
+/**
+ *
+ * @author ayushighia
+ */
+public class Person {
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phone;
+    private String driversLicenseNumber;
+    private String driversLicenseState;
+    private String socialSecurityNumber;
+    private String addressLine1;
+    private String addressLine2;
+    private String city;
+    private String state;
+    private String zip;
+    private String telephoneNumber;
+    private String faxNumber;
+    
+   
+    private String gender;
+    private String noofkids;
+    private String maritalstatus;
+    private String noofsiblings;
+    private String occupation;
+    private String alternamenumber;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getDriversLicenseNumber() {
+        return driversLicenseNumber;
+    }
+
+    public void setDriversLicenseNumber(String driversLicenseNumber) {
+        this.driversLicenseNumber = driversLicenseNumber;
+    }
+
+    public String getDriversLicenseState() {
+        return driversLicenseState;
+    }
+
+    public void setDriversLicenseState(String driversLicenseState) {
+        this.driversLicenseState = driversLicenseState;
+    }
+
+    public String getSocialSecurityNumber() {
+        return socialSecurityNumber;
+    }
+
+    public void setSocialSecurityNumber(String socialSecurityNumber) {
+        this.socialSecurityNumber = socialSecurityNumber;
+    }
+
+    public String getAddressLine1() {
+        return addressLine1;
+    }
+
+    public void setAddressLine1(String addressLine1) {
+        this.addressLine1 = addressLine1;
+    }
+
+    public String getAddressLine2() {
+        return addressLine2;
+    }
+
+    public void setAddressLine2(String addressLine2) {
+        this.addressLine2 = addressLine2;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getZip() {
+        return zip;
+    }
+
+    public void setZip(String zip) {
+        this.zip = zip;
+    }
+
+    public String getTelephoneNumber() {
+        return telephoneNumber;
+    }
+
+    public void setTelephoneNumber(String telephoneNumber) {
+        this.telephoneNumber = telephoneNumber;
+    }
+
+    public String getFaxNumber() {
+        return faxNumber;
+    }
+
+    public void setFaxNumber(String faxNumber) {
+        this.faxNumber = faxNumber;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getNoofkids() {
+        return noofkids;
+    }
+
+    public void setNoofkids(String noofkids) {
+        this.noofkids = noofkids;
+    }
+
+    public String getMaritalstatus() {
+        return maritalstatus;
+    }
+
+    public void setMaritalstatus(String maritalstatus) {
+        this.maritalstatus = maritalstatus;
+    }
+
+    public String getNoofsiblings() {
+        return noofsiblings;
+    }
+
+    public void setNoofsiblings(String noofsiblings) {
+        this.noofsiblings = noofsiblings;
+    }
+
+    public String getOccupation() {
+        return occupation;
+    }
+
+    public void setOccupation(String occupation) {
+        this.occupation = occupation;
+    }
+
+    public String getAlternamenumber() {
+        return alternamenumber;
+    }
+
+    public void setAlternamenumber(String alternamenumber) {
+        this.alternamenumber = alternamenumber;
+    }
+    
+   
+}
+
+
